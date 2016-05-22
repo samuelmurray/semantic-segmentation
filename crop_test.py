bc225ce641a9df7836874a9b2766d5b4cf2fbd35
Samuel Murray <samuel.murray@outlook.com>
2016-05-22 15:53:34 +0200

    File to test image cropping
    Needs python pillow package installed

---
+++ crop_test.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from crop_util import *
+
+image_folder = './imagenet/'
+image_name = 'panda'
+image_decoding = '.jpeg'
+image = Image.open(image_folder + image_name + image_decoding)
+print(type(image))
+width, height = image.size
+print("w = {}, h = {}".format(width, height))
+
+max_images = 500
+generate_and_save_crops(image, image_name, max_num_images=30)
