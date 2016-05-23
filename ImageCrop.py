f98f1314c12107c8a56da82cf830232099a8d60c
Kristofer Hannesson <kristoh@gmail.com>
2016-05-23 22:17:55 +0200

    added __slots__ to save memory, also added 'area' variable
    we will be working with tens of thousands of these so it is good to
    save memory if we can

--- ImageCrop.py
+++ ImageCrop.py
@@ -3,9 +3,12 @@ class ImageCrop:
     Represents a crop of an image, with stored values of the coordinates in the original image.
     Used to compare the crop to bounding boxes of objects in the original image.
     """
+    __slots__ = ['image', 'x_min', 'y_min', 'x_max', 'y_max', 'area']
+
     def __init__(self, image, x_min: int, y_min: int, x_max: int, y_max: int):
         self.image = image
         self.x_min = x_min  # type: int
         self.y_min = y_min  # type: int
         self.x_max = x_max  # type: int
         self.y_max = y_max  # type: int
+        self.area = (x_max - x_min) * (y_max - y_min)  # type: int
