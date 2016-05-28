993bbf8a0c230b35c09467b77d0c3e8966c112fe
Kristofer Hannesson <kristoh@gmail.com>
2016-05-28 21:35:54 +0200

    testing actual images

--- test_conv_net_feed.py
+++ test_conv_net_feed.py
@@ -4,6 +4,7 @@ from __future__ import division
 import numpy as np
 import conv_net_feed as cnnfeed
 import conv_net_util as cnnutil
+from utilities import training_images, training_labels, validation_images, validation_labels
 
 # Define the data
 image = 'imagenet/panda.jpeg'
@@ -11,8 +12,12 @@ image2 = 'imagenet/panda-update.jpg'
 image_names = [image, image2, image]
 batch_size = len(image_names)
 
+image_names = training_images[0:100]
+image_labels = training_labels[0:100]
+
+
 # Preprocess data
 image_data = cnnutil.preprocess_images(image_names)
 image_labels = np.array([0, 6, 0])  # Make sure to use the same label for identical images
 
-cnnfeed.run_training(image_data, image_labels, image_data, image_labels)
+cnnfeed.run_training(image_data, image_labels, validation_images[0:10], validation_labels[0:10])
