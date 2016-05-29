0a63307f93735aef8b7d4230e5fcb581e5120f07
Kristofer Hannesson <kristoh@gmail.com>
2016-05-29 21:49:55 +0200

    testing

--- preprocess_images.py
+++ preprocess_images.py
@@ -24,35 +24,35 @@ def parse_args():
     return args
 
 
-for label in chosen_labels:
-    train_names = train_images_by_label[label]
-    val_names = val_images_by_label[label]
+# for label in chosen_labels:
+#     train_names = train_images_by_label[label]
+#     val_names = val_images_by_label[label]
+#
+#     start_time = time.time()
+#     print("starting on training: ", label)
+#     cnnfeed.save_image_output(train_names, image_type="training")
+#     print("starting on validation: ", label)
+#     cnnfeed.save_image_output(val_names, image_type="validation")
+#     print("Saving {} train and {} val images took {}s".format(len(train_names), len(val_names), time.time() - start_time))
 
-    start_time = time.time()
-    print("starting on training: ", label)
-    cnnfeed.save_image_output(train_names, image_type="training")
-    print("starting on validation: ", label)
-    cnnfeed.save_image_output(val_names, image_type="validation")
-    print("Saving {} train and {} val images took {}s".format(len(train_names), len(val_names), time.time() - start_time))
+args = parse_args()
+index = int(args.index)
+label = chosen_labels[index]
 
-# args = parse_args()
-# index = int(args.index)
-# label = chosen_labels[index]
-#
-# train_names = train_images_by_label[label]
-# val_names = val_images_by_label[label]
-#
-# random.shuffle(train_names)
-# random.shuffle(val_names)
-#
+train_names = train_images_by_label[label]
+val_names = val_images_by_label[label]
+
+random.shuffle(train_names)
+random.shuffle(val_names)
+
 # train_names = train_names[0:3000]
 # val_names = val_names[0:1000]
 
 
-#start_time = time.time()
-#cnnfeed.save_image_output(train_names, image_type="training")
-#cnnfeed.save_image_output(val_names, image_type="validation")
-#print("Saving {} train and {} val images took {}s".format(len(train_names), len(val_names), time.time() - start_time))
+start_time = time.time()
+cnnfeed.save_image_output(train_names, image_type="training")
+cnnfeed.save_image_output(val_names, image_type="validation")
+print("Saving {} train and {} val images took {}s".format(len(train_names), len(val_names), time.time() - start_time))
 
 # # Preprocess data
 # train_data = cnnutil.preprocess_images(train_names)
