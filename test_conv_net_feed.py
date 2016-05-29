5d4677be95827294a537dce6918e92cc2bb3d822
Kristofer Hannesson <kristoh@gmail.com>
2016-05-29 09:37:33 +0200

    can set how many training and validation images to use

--- test_conv_net_feed.py
+++ test_conv_net_feed.py
@@ -16,12 +16,13 @@ image_labels = np.array([0, 6, 0])  # Make sure to use the same label for identi
 image_data = cnnutil.preprocess_images(image_names)
 cnnfeed.run_training(image_data, image_labels, image_data, image_lables)
 """
-how_many = 1000
+how_many_train = 1000000000  # set to large number to take all
+how_many_val = 10000000  # set to large number to take all
 
-train_names = training_images_small[0:how_many]
-train_labels = training_labels_small[0:how_many]
-val_names = validation_images_small[0:10]
-val_labels = validation_labels_small[0:10]
+train_names = training_images_small[0:how_many_train]
+train_labels = training_labels_small[0:how_many_val]
+val_names = validation_images_small[0:how_many_val]
+val_labels = validation_labels_small[0:how_many_val]
 
 # Preprocess data
 train_data = cnnutil.preprocess_images(train_names)
